918316e3a24c989b07f78349f9662d27f373019e
Arun Saradgi <saradgi07@gmail.com>
2022-12-30 01:24:16 +0530

    basic factory function

--- unit-3 pra/objectsrelated(5,6,7)/factoryfunc.js
+++ unit-3 pra/objectsrelated(5,6,7)/factoryfunc.js
@@ -2,17 +2,26 @@
 // factory function create and return obj
 
 function createAcc(accname,acctype,balance,active){
-    let acc={
-        name:accname,
-        type:acctype,
-        balance:balance,
-        active:active,
+    //..short hand and returning directly
+    return{
+        accname,acctype,balance,active,
         print:function(){
-            console.log(`${this.name} has balance of ${balance}`);
+            return (`${this.accname} has balance of ${this.balance}`);
         }
     }
-    return acc;
+
+    //.. full declaration 
+    // let acc={
+    //     name:accname,
+    //     type:acctype,
+    //     balance:balance,
+    //     active:active,
+    //     print:function(){
+    //         console.log(`${this.name} has balance of ${balance}`);
+    //     }
+    // }
+    // return acc;
 }
 
 console.log(createAcc('arun','savings',500,true))
-console.log(createAcc('arun','savings',500,true).print())
+console.log(createAcc('arun','savings',300,true).print())
